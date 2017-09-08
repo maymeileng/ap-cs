5dd3ce970e0e67455019db12576cb8bb122c9a18
maymeileng <maymeileng@gmail.com>
2017-09-07 21:20:54 -0700

    Flag Project

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 		public static void main(String[] args) {
-			//Creates the flag
+			//Create flag
 			Flag g = new Flag();
 			g.setDefaultCloseOperation(g.EXIT_ON_CLOSE);
 			g.setVisible(true);
