896f0261e4e4a0d8dea00049bd02386756497942
maymeileng <maymeileng@gmail.com>
2017-11-28 21:54:28 -0800

    Three sorts

--- Sort.java
+++ Sort.java
@@ -32,7 +32,7 @@ public class Sort {
 		 int n = array.length;
 		 int temp = 0;
 		 for(int i = 0; i < n; i++){
-			 for(int j = 1; j < (n - i); j++){
+			 for (int j = 1; j < (n - i); j++) {
 				 if (array[j-1] > array[j]) {
 					 temp = array[j-1];
 					 array[j-1] = array[j];
